43aa4fc3f669fd9b4917e35da2e4b414b01386db
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-06 16:11:01 +0300

    add Homework 008

---
+++ Homework008/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8
+// Показать четные числа от 1 до N
+
+Console.Write("Введите число N: ");
+string userNumber = Console.ReadLine();
+double number = int.Parse(userNumber);
+
+for (int initialNum = 2; initialNum < number; initialNum++)
+{
+    if (initialNum % 2 == 0)
+    {
+        Console.Write(initialNum + " ");
+    }
+}
