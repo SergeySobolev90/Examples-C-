628b75cfa27b9dd08ea690bbfb3a6da1722019be
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-06 13:13:39 +0300

    add HomeWork 002

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 2 
+// Даны два числа. Показать большее и меньшее число.
+
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(0, 100);
+    return randomNumber;
+}
+
+int firstNumber = GetRandomNumber();
+int secondNumber = GetRandomNumber();
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"Большее число: {firstNumber} Меньшее число: {secondNumber}");
+}
+else
+{
+    Console.WriteLine($"Большее число: {secondNumber} Меньшее число: {firstNumber}");
+}
+
+
