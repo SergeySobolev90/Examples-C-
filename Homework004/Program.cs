81fb4c1213a86a85a8cdb594576d862a52bbf594
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-06 13:36:02 +0300

    add Homework 004

---
+++ Homework004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4 
+//Найти максимальное из трех чисел
+
+int GetRandomNumber()
+{
+    int randomNumber = new Random().Next(0,100);
+    return randomNumber;
+}
+
+int numOne = GetRandomNumber();
+int numTwo = GetRandomNumber();
+int numThree = GetRandomNumber();
+int max = numOne;
+if(numOne > max) max = numOne;
+if(numTwo > max) max = numTwo;
+if(numThree > max) max = numThree;
+
+Console.WriteLine($"Максимальное из трех чисел: {max}");
