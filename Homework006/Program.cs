4fd9da70abfb783559d4f8202b8b6795f31766fc
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-06 14:20:54 +0300

    add Homework 006

---
+++ Homework006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6 
+//Выяснить является ли число чётным
+
+Console.Write("Введите любое целое число: ");
+string userNumber = Console.ReadLine();
+double numDouble = int.Parse(userNumber);
+if (numDouble % 2 == 0)
+{
+    Console.WriteLine($"{numDouble} четное число.");
+}
+else
+{
+    Console.WriteLine($"{numDouble} не четное число.");
+}
+
