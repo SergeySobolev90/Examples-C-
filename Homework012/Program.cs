4f397650460046117d5eea6c5b91decf09909dc0
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-07 17:27:54 +0300

    add Homework 012

---
+++ Homework012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 12 
+// Удалить вторую цифру трехзначного числа 
+
+int GetRandom()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+
+int firstDigit = randomazer / 100;
+int lastDigit = randomazer % 10;
+Console.WriteLine($"{firstDigit}{lastDigit}");
