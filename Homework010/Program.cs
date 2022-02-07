b36181c5c2facd14515c9434b8e74251a6eb4a80
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-07 17:07:30 +0300

    add Homework 010

---
+++ Homework010/Program.cs
@@ -0,0 +1,13 @@
+﻿// 10 Задача
+// Показать вторую цифру трехзначного числа
+
+int GetRandom()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число {randomazer}");
+
+int result = randomazer % 100 / 10;
+Console.WriteLine($" Вторая цифра трехзначного числа:  {result}");
