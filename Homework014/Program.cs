f8839d26860b5fc36a63f49b552fb4713c26d3c5
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-07 18:30:22 +0300

    add Homework 014

---
+++ Homework014/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 14 
+// Найти третью цифру числа или сообщить, что ее нет 
+
+int GetRandom()
+{
+    int number = new Random().Next(0, 1000);
+    return number;
+}
+int randomazer = GetRandom();
+Console.WriteLine($"Случайное число: {randomazer}");
+
+int noThreeDigit = randomazer / 10;
+int threeDigit = randomazer % 10;
+if (noThreeDigit >= 10)
+{
+    Console.WriteLine($"Третья цифра числа: {threeDigit}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет ");
+}
