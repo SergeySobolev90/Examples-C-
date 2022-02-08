414f2f65e6652fb8c18ebe0d307def010ab80f7f
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-08 18:24:03 +0300

    add Homework 010 new code

--- Homework010/Program.cs
+++ Homework010/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 10 Задача
 // Показать вторую цифру трехзначного числа
 
-int GetRandom()
+/*int GetRandom()
 {
     int number = new Random().Next(100, 1000);
     return number;
@@ -10,4 +10,24 @@ int randomazer = GetRandom();
 Console.WriteLine($"Случайное число {randomazer}");
 
 int result = randomazer % 100 / 10;
-Console.WriteLine($" Вторая цифра трехзначного числа:  {result}");
+Console.WriteLine($" Вторая цифра трехзначного числа:  {result}"); */
+
+
+Console.Write("Введите трехзначное число: ");
+
+ string threeDigitNumStr = Console.ReadLine();
+ if(int.TryParse(threeDigitNumStr, out int threeDigitNumInt))
+ {
+     if(threeDigitNumStr.Length >= 3)
+    {
+        Console.WriteLine($"Вторая цифра числа: {threeDigitNumStr[1]}");
+    }
+     else
+     {
+         Console.WriteLine("Число не трехзначное.");
+     }
+ }
+  else
+  {
+      Console.WriteLine("Ввели не число.");
+  }
