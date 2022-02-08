5ccc828f1e716e2ab9997372319de1baf079258d
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-08 15:55:14 +0300

    add Homework 006 check in line 6

--- Homework006/Program.cs
+++ Homework006/Program.cs
@@ -2,14 +2,12 @@
 //Выяснить является ли число чётным
 
 Console.Write("Введите любое целое число: ");
-string userNumber = Console.ReadLine();
-double numDouble = int.Parse(userNumber);
-if (numDouble % 2 == 0)
+string valueStr = Console.ReadLine();
+if(int.TryParse(valueStr, out int valueInt))
 {
-    Console.WriteLine($"{numDouble} четное число.");
-}
-else
-{
-    Console.WriteLine($"{numDouble} не четное число.");
+    if (valueInt % 2 == 0)
+    Console.WriteLine($"{valueStr} четное число.");
+    else
+    Console.WriteLine($"{valueStr} не четное число.");
 }
 
