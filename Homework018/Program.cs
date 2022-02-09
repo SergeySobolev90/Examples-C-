c3fec4ca5b665cd8c15d97d8c933f5fa26d512f2
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-09 20:46:16 +0300

    add Homework 018

---
+++ Homework018/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 18 
+//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+// ¬ знак отрицания 
+// ⋁ логическое "И" дизьюнкция 
+// ⋀ логическое "ИЛИ" коньюнция
+
+void TruthOfTheStatements( bool x, bool y)
+{
+    Console.WriteLine($"!({x} || {y}) = {!(x || y)}, !{x} && !{y} = {!x && !y}");
+}
+
+TruthOfTheStatements(true, false);
+TruthOfTheStatements(false, true);
+TruthOfTheStatements(true, true);
+TruthOfTheStatements(false, false);
+
