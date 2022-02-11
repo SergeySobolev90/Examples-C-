02bce1a9e8b878d3bc6a4c402aa4f33cb46cff9f
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-11 15:56:26 +0300

    add Homework 010 formating

--- Homework010/Program.cs
+++ Homework010/Program.cs
@@ -14,20 +14,13 @@ Console.WriteLine($" Вторая цифра трехзначного числа
 
 
 Console.Write("Введите трехзначное число: ");
-
- string threeDigitNumStr = Console.ReadLine();
- if(int.TryParse(threeDigitNumStr, out int threeDigitNumInt))
- {
-     if(threeDigitNumStr.Length >= 3)
-    {
+string threeDigitNumStr = Console.ReadLine();
+if (int.TryParse(threeDigitNumStr, out int threeDigitNumInt))
+{
+    if (threeDigitNumStr.Length >= 3)
         Console.WriteLine($"Вторая цифра числа: {threeDigitNumStr[1]}");
-    }
-     else
-     {
-         Console.WriteLine("Число не трехзначное.");
-     }
- }
-  else
-  {
-      Console.WriteLine("Ввели не число.");
-  }
+    else
+        Console.WriteLine("Число не трехзначное.");
+}
+else
+    Console.WriteLine("Ввели не число.");
