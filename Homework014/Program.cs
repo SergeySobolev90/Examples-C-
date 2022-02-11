c13a627157be5f91f816eea1dce4f83ed2f35065
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-11 15:59:59 +0300

    add Homework014 fotmating

--- Homework014/Program.cs
+++ Homework014/Program.cs
@@ -3,20 +3,14 @@
 
 Console.Write("Введите число: ");
 
- string arbitraryNumberStr = Console.ReadLine();
- if(int.TryParse(arbitraryNumberStr, out int arbitraryNumberInt))
- {
-     if(arbitraryNumberStr.Length >= 3)
-    {
+string arbitraryNumberStr = Console.ReadLine();
+if (int.TryParse(arbitraryNumberStr, out int arbitraryNumberInt))
+{
+    if (arbitraryNumberStr.Length >= 3)
         Console.WriteLine($"Третья цифра числа: {arbitraryNumberStr[2]}");
-    }
-     else
-     {
-         Console.WriteLine("Число не трехзначное.");
-     }
- }
-  else
-  {
-      Console.WriteLine("Ввели не число.");
-  }
+    else
+        Console.WriteLine("Число не трехзначное.");
+}
+else
+    Console.WriteLine("Ввели не число.");
 
