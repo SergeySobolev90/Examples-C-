fdd0599f7ebe9221de7da754b5e65d60145d6478
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-11 19:13:27 +0300

    add Homework 024

---
+++ Homework024/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Задача 24
+// Найти кубы чисел от 1 до N
+
+Console.Write("Введите целое число: ");
+int randomNumber = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= randomNumber; i++)
+    Console.Write($"{i * i * i} ");
+
