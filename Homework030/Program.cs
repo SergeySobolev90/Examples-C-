e789e68073f2351ae9cdc594418c2d13a1ed268e
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-15 22:07:09 +0300

    add Homework 030

---
+++ Homework030/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 30 
+// Показать кубы чисел, заканчивающихся на четную цифру
+
+int randomNumber = new Random().Next(1, 100);
+
+for (int i = 1; i <= randomNumber; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i * i * i} ");
+    }
+}
+
