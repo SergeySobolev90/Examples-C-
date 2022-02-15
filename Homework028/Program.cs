ac7436d346a6bf0af9b21f3bc0a9db7f7066731f
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-15 21:19:35 +0300

    add Homework 028

---
+++ Homework028/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28
+// Посчитать сумму цифр в числе
+
+
+int DigitsSum(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Сумма цифр: " + DigitsSum(n));
+
