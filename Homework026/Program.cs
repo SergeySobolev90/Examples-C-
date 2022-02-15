4a6b6df9dd4cb2b1c8b751d7ad5076db9e522262
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-15 21:18:37 +0300

    add Homework 026

---
+++ Homework026/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Задача 26 
+// Возвести число А в натуральную степень числа В используя цикл 
+
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int numberB = int.Parse(Console.ReadLine());
+int numberA_numberB = 1;
+for (int i = 0; i < numberB; i++)
+    numberA_numberB *= numberA;
+Console.WriteLine("{0} ^ {1} = {2}", numberA, numberB, numberA_numberB);
+
