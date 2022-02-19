9ab84e33076c395bb2c2729f6ac4fa9b1d84cb05
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-19 19:13:14 +0300

    add Homework 040

---
+++ Homework040/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 40 
+// В Указанном массиве вещественных чисел 
+//найдите разницу между максимальным и минимальным элементом
+
+double[] arr = new double[10];
+
+void FullArray()
+{
+    Random ran = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = ran.NextDouble() * 100;
+        Console.Write("{0,6:F2}", arr[i]);
+    }
+}
+
+void Difference()
+{
+    double max = 0;
+    double min = arr[0];
+    double diff = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+        if (min > arr[i])
+            min = arr[i];
+    }
+    diff = max - min;
+    Console.WriteLine($"Разница между max {max} и min {min} = {diff}");
+}
+
+FullArray();
+Console.WriteLine();
+Difference();
