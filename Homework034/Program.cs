d1dc2b470beb059472ecd8b1c26ce625436a9ebc
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-19 19:11:35 +0300

    add Homework 034

---
+++ Homework034/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34 
+// Написать программу замену элементов массива на противоположные
+
+int[] arr = new int[10];
+
+void FullArray()
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 10) - 5;
+        Console.Write($"{arr[i]}  ");
+    }
+
+}
+
+void ReplaceNum()
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -1 * arr[i];
+        Console.Write($"{arr[i]}  ");
+    }
+}
+
+FullArray();
+Console.WriteLine();
+ReplaceNum();
