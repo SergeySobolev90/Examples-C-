a7ea6a1590eb2f3f2222c427e8d6b300a8ba10d2
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-19 19:12:32 +0300

    add Homework 038

---
+++ Homework038/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 38
+// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+
+int[] arr = new int[20];
+
+void FullArray()
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 9);
+        Console.Write($"{arr[i]} ");
+    }
+
+}
+
+void Summ()
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            sum += arr[i];
+    }
+    Console.WriteLine($"Cумма чисел стоящих на нечетной позиции: {sum}");
+}
+
+Console.Write("Одномерный массив: ");
+FullArray();
+Console.WriteLine();
+Summ();
