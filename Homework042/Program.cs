28ac472215cc8d371dec4b899ea7464dd48abb52
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-23 16:53:07 +0300

    add Homework 042

---
+++ Homework042/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42 
+// Определить сколько чисел больше 0 введено с клавиатуры
+
+int numbers = 5;
+int count = 0;
+while (numbers > 0)
+{
+    Console.Write("Введите число: ");
+    string valueStr = Console.ReadLine();
+    if (int.TryParse(valueStr, out int valueInt))
+    {
+        if (valueInt > 0)
+        {
+            count++;
+        }
+        numbers--;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0, введенных с клавиатуры = {count}");
+
+
+
+
+
+
+
