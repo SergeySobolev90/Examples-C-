61c0723764d8f163a82ca5c05941296d7d1262a7
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-27 13:14:19 +0300

    add Homework 032 вывели рандом за пределы цикла

--- Homework032/Program.cs
+++ Homework032/Program.cs
@@ -3,8 +3,10 @@
 
 int[] array = new int[8];
 Console.WriteLine("Массив из 8 элементов:");
+
+Random ran = new Random();
 for (int i = 0; i < 8; i++)
 {
-    array[i] = new Random().Next(0,2);
+    array[i] = ran.Next(0, 2);
     Console.Write($"{array[i]}");
 }
