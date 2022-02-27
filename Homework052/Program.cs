309adfbd9bc7c85d6d91a5915aaea13cb05576d1
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-27 14:35:12 +0300

    add Homework 052

---
+++ Homework052/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52 
+// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+
+Random rand = new Random();
+int[,] FillArray()
+{
+    int[,] arr = new int[5, 5];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rand.Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Replace(int[,] arrSecond)
+{
+    for (int i = 0; i < arrSecond.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrSecond.GetLength(1); j++)
+        {
+            if (arrSecond[i, j] % 2 == 0)
+                arrSecond[i, j] = arrSecond[i, j] * arrSecond[i, j];
+        }
+    }
+    return arrSecond;
+}
+
+int[,] fillArr = FillArray();
+PrintArray(fillArr);
+Console.WriteLine();
+int[,] RepMat = Replace(fillArr);
+PrintArray(RepMat);
