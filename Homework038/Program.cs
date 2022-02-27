31c9913f5c0caee53dabaf56d90ee944af29e0ab
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-27 13:18:26 +0300

    add Homework038 вывели рандом за пределы цикла

--- Homework038/Program.cs
+++ Homework038/Program.cs
@@ -6,9 +6,10 @@ int[] arr = new int[20];
 
 void FullArray()
 {
+    Random ran = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(1, 9);
+        arr[i] = ran.Next(1, 9);
         Console.Write($"{arr[i]} ");
     }
 
