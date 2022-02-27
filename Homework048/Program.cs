b1693a691cd47b458779f5a86411e0de991ae69b
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-02-27 14:34:09 +0300

    add Homework 048

---
+++ Homework048/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 48
+// Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] arr = new int[5, 5];
+
+Random rand = new Random();
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
