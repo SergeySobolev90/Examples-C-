baf8c9e5843934487e4465d6699957c156fc0717
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-03-03 16:23:06 +0300

    add Homework 063

---
+++ Homework063/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 63 
+// Сформировать трехмерный массив не повторяющимися двузначными числами
+// показать его построчно на экран выводя индексы соответствующего элемента
+
+Random rand = new Random();
+int[,,] FillArray()
+{
+    int[,,] array = new int[3, 3, 3];
+    rand.Next(10, 20);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rand.Next(10, 20);
+
+            }
+        }
+    }
+    return array;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"[{i},{j},{k}] = {array[i, j, k]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+int[,,] arr = FillArray();
+Console.WriteLine();
+PrintArray(arr);
+
+
